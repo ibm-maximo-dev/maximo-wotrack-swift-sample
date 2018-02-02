39b231ef5139a0d9dc42c6cd5c6b98f620f4e3a0
Silvino Vieira de Vasconcelos Neto <silvv@br.ibm.com>
2018-02-02 19:07:52 -0300

    Creating the WOTRACK sample app in Swift.

---
+++ MaximoWOTRACKSample/MaximoWOTRACKSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaximoWOTRACKSample
+//
+//  Created by Silvino Vieira de Vasconcelos Neto on 02/02/2018.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MaximoWOTRACKSample/MaximoWOTRACKSample/WorkOrderTableView.swift
@@ -0,0 +1,9 @@
+//
+//  WorkOrderTableView.swift
+//  MaximoWOTRACKSample
+//
+//  Created by Silvino Vieira de Vasconcelos Neto on 02/02/2018.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import Foundation
