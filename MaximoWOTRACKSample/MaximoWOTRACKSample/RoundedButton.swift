69f21e1368357da4c26adff24c8a7c7296dbee3c
Silvino Vieira de Vasconcelos Neto <silvv@br.ibm.com>
2018-02-08 20:21:03 -0300

    Improving usability and UI design.

---
+++ MaximoWOTRACKSample/MaximoWOTRACKSample/RoundedButton.swift
@@ -0,0 +1,9 @@
+//
+//  RoundedButton.swift
+//  MaximoWOTRACKSample
+//
+//  Created by Silvino Vieira de Vasconcelos Neto on 08/02/2018.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import Foundation
