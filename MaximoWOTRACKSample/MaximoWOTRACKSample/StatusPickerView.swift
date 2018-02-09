1869f5e7a508a68dcdb3e3e24bc7b15fb981a7dd
Silvino Vieira de Vasconcelos Neto <silvv@br.ibm.com>
2018-02-09 12:58:55 -0300

    Implementing status change.

---
+++ MaximoWOTRACKSample/MaximoWOTRACKSample/StatusPickerView.swift
@@ -0,0 +1,9 @@
+//
+//  StatusPickerView.swift
+//  MaximoWOTRACKSample
+//
+//  Created by Silvino Vieira de Vasconcelos Neto on 09/02/2018.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import Foundation
