73d4b27fb1748ce59d02f4aeb2e79f038fcb31f9
harikrishna <harikrishna@us.ibm.com>
2018-07-30 16:22:36 -0400

    add to gitignore

--- MaximoWOTRACKSample/Pods/Target Support Files/MaximoRESTSDK/MaximoRESTSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MaximoRESTSDK : NSObject
-@end
-@implementation PodsDummy_MaximoRESTSDK
-@end
--- MaximoWOTRACKSample/Pods/Target Support Files/Pods-MaximoWOTRACKSampleTests/Pods-MaximoWOTRACKSampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MaximoWOTRACKSampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_MaximoWOTRACKSampleTests
-@end
